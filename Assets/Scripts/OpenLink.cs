b828f1d8c6d0e6d25e55c453f442087c4986c77b
MirunaCojocaru26 <mirunacojocaru26@yahoo.com>
2020-04-02 18:48:56 +0300

    AR finish

---
+++ Assets/Scripts/OpenLink.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void Open()
+    {
+        Application.OpenURL("http://cariere.as-mi.ro");
+    }
+}
