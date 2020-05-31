b1ceb635316835f37569f59574762ad267f734e3
MirunaCojocaru26 <mirunacojocaru26@yahoo.com>
2020-05-31 13:01:28 +0300

    json2

---
+++ Assets/Vuforia/Scripts/jsonController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jsonController : MonoBehaviour
+{
+    public string jsonURL;
+    public jsonDataClass jsnData;
+    void Start()
+    {
+        StartCoroutine(getData());
+    }
+
+    IEnumerator getData()
+    {
+        WWW _www = new WWW(jsonURL);
+        yield return _www;
+        if(_www.error == null)
+        {
+            processJsonData(_www.text);
+        }
+        else
+        {
+            Debug.Log("Oops, something went wrong");
+        }
+    }
+
+    private void processJsonData(string _url)
+    {
+        jsnData = JsonUtility.FromJson<jsonDataClass>(_url);
+    }
+}
---
+++ Assets/Vuforia/Scripts/jsonDataClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+
+[Serializable]
+public class jsonDataClass
+{
+    public List<infoList> info;
+}
+
+[Serializable]
+public class infoList
+{
+    public string data;
+}
